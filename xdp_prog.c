3a069a1127b0ccf8c4a5e77a435513bac367a36f
josefzeman <pepa.elektronik@gmail.com>
2025-04-08 12:01:26 +0200

    Cookie check, Checksum

--- xdp_prog.c
+++ xdp_prog.c
@@ -4,6 +4,7 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <bpf/bpf_endian.h>
 
 struct {
     __uint(type, BPF_MAP_TYPE_LRU_HASH);
@@ -12,7 +13,8 @@ struct {
     __type(value, __u32); // cookie
 } cookie_map SEC(".maps");
 
-static __u32 (*bpf_tcp_raw_gen_syncookie)(struct iphdr *iph, struct tcphdr *th, __u32 th_len) = (void *) BPF_FUNC_tcp_raw_gen_syncookie_ipv4; // helper for cookie generation
+static __u32 (*bpf_tcp_raw_gen_syncookie_v4)(struct iphdr *iph, struct tcphdr *th, __u32 th_len) = (void *) BPF_FUNC_tcp_raw_gen_syncookie_ipv4; // helper for cookie generation ipv4
+static __u32 (*bpf_tcp_raw_gen_syncookie_v6)(struct iphdr *iph, struct tcphdr *th, __u32 th_len) = (void *) BPF_FUNC_tcp_raw_gen_syncookie_ipv6; // helper for cookie generation ipv6
 
 SEC("xdp_syn_cookie")
 int handle_syn(struct xdp_md *ctx) {
@@ -29,50 +31,57 @@ int handle_syn(struct xdp_md *ctx) {
         if((void *)(ip + 1) > data_end) return XDP_PASS;
 
         struct tcphdr *tcp = (struct tcphdr*)(ip + 1); // tcp
-        if((void *)(tcp + 1) > data_end) return XDP_PASS;\
+        if((void *)(tcp + 1) > data_end) return XDP_PASS;
 
-        if(tcp->syn && !tcp->ack){ // check syn packet
+        if(tcp->syn && !tcp->ack){ // is packet syn && !ack?
 
             __u32 cookie;
-        
+       
             // generate SYN cookie
-            cookie = bpf_tcp_raw_gen_syncookie(ip, tcp, sizeof(*tcp));
+            cookie = bpf_tcp_raw_gen_syncookie_v4(ip, tcp, sizeof(*tcp));
             if (!cookie) return XDP_DROP;
 
 
             bpf_map_update_elem(&cookie_map, &ip->saddr, &cookie, BPF_ANY); // save cookie
 
+            tcp->ack = 1;
+            tcp->syn = 1;
+            tcp->seq = 0;
+            tcp->ack_seq = bpf_htonl(bpf_ntohl(tcp->seq) + 1);
 
-            /* 
+            tcp->check = 0;
+            tcp->check = bpf_csum_diff((__be32 *)&ip->saddr, sizeof(ip->saddr), (__be32 *)&ip->daddr, sizeof(ip->daddr), 0);
+            tcp->check = bpf_csum_diff((__be32 *)&tcp, sizeof(*tcp), 0, 0, tcp->check);
 
-                end answer (SYN+ACK) with cookie
-                hecksums
-                pseudoH
+            ip->check = 0;
+            ip->check = bpf_csum_diff((__be32 *)&ip, sizeof(*ip), 0, 0, 0);
 
-            */
+            return XDP_TX;
 
+        } else if(tcp->ack && !tcp->syn){ // is packet ack && !syn?
+            __u32 *cookieExists = bpf_map_lookup_elem(&cookie_map, &ip->saddr);
 
-            /*
+            if (!cookieExists) return XDP_DROP;
 
-                ACK expected cookie check -> XDP_DROP || XDP_PASS
-                delete cookie from map
+            __u32 cookieRec = bpf_ntohl(tcp->seq);
+            if (cookieRec != *cookieExists) return XDP_DROP;
 
-            */
-        } else if(tcp->ack && !tcp->syn){ // check ack packet
+            bpf_map_delete_elem(&cookie_map, &ip->saddr);
 
+            return XDP_PASS;
 
+        } else {
+            return XDP_DROP;
         }
 
-    // IPv6 
+    // IPv6
     } else if(eth->h_proto == __constant_htons(ETH_P_IPV6)) {
-
-
+        
     }
-
-
 
 
     return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
+
